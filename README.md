2d037e6b91320d80d18c38d6d4437eb91f4dfe4f
qinwenchao <qinwenchao@chunyu.me>
2016-08-17 21:42:13 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,72 @@
 # python_config_auto_generate
-use python to auto generate config files by template files with different user defined env_variables 
+use python to auto generate config files by template files with different user defined env variables
+
+initial conf structure
+conf
+    env
+        default.config
+        local.config
+        test.config
+        online.config
+    rpc(has template file)
+        config.template.ini
+    supervisor(has template file)
+        supervisord.template.conf
+    settings(has no template file just ignore)
+        a.txt
+    uwsgi(has template file)
+        uwsgi.template.xml
+
+
+step 1:
+    write template file. use ${VAR_XX} to define env variable(refer to config.template.ini)
+step 2:
+    write default config and specific config, assign value to env variable(make sure all variables are not "")
+setp 3:
+    run the script with two ways:
+    3.1 generate all config files
+        python config_manager.py conf conf/env
+        conf
+            env
+                default.config
+                local.config
+                test.config
+                online.config
+            rpc(has template file)
+                config.local.ini(auto generate)
+                config.test.ini(auto generate)
+                config.online.ini(auto generate)
+                config.template.ini
+            supervisor(has template file)
+                supervisord.local.conf(auto generate)
+                supervisord.test.conf(auto generate)
+                supervisord.online.conf(auto generate)
+                supervisord.template.conf
+            settings(has no template file just ignore)
+                a.txt
+            uwsgi(has template file)
+                uwsgi.local.xml(auto generate)
+                uwsgi.test.xml(auto generate)
+                uwsgi.online.xml(auto generate)
+                uwsgi.template.xml
+
+    3.2 generate specific config files
+        python config_manager.py conf conf/env online
+
+        conf
+            env
+                default.config
+                local.config
+                test.config
+                online.config
+            rpc(has template file)
+                config.online.ini(auto generate)
+                config.template.ini
+            supervisor(has template file)
+                supervisord.online.conf(auto generate)
+                supervisord.template.conf
+            settings(has no template file just ignore)
+                a.txt
+            uwsgi(has template file)
+                uwsgi.online.xml(auto generate)
+                uwsgi.template.xml
---
+++ config_manager.py
@@ -0,0 +1,137 @@
+# -*- coding:utf-8 -*-
+import sys
+
+import os
+import re
+
+
+class ConfigManager(object):
+    """
+    config manager, read env variable: default one and specific one,
+    auto generate config file according to template file
+    replace ${XX} by env variable
+    also do the validation in case of env variable is ""
+    """
+
+    def __init__(self, env_default_file_path, env_specific_file_path):
+        super(ConfigManager, self).__init__()
+        self.env_config = {}
+        self.load = False
+        self.env_default_file_path = env_default_file_path
+        self.env_specific_file_path = env_specific_file_path
+
+    def init_env_config(self):
+        """
+        first load default config and then load specific config
+        """
+        self.env_config.clear()
+        self.read_config(self.env_default_file_path)
+        self.read_config(self.env_specific_file_path)
+        self.validate_config()
+        self.load = True
+
+    def read_config(self, file_path):
+        """
+        读取配置文件信息
+        """
+        with open(file_path) as f:
+            for line in f.readlines():
+                line = line.strip()
+                if not line:
+                    continue
+                # annotation pass
+                if line.startswith("#"):
+                    continue
+                items = line.strip().split("=")
+                item_key = items[0].strip()
+                item_value = items[1].strip()
+                self.env_config[item_key] = item_value
+
+    def validate_config(self):
+        """
+        env_config value can not be empty string(in case forget to assign value)
+        """
+        for key, value in self.env_config.items():
+            assert value != ""
+
+    def render_config_file(self, template_file_path, out_file_path):
+        """
+        replace template ${xx} with env variable xx, then generate new config file
+        """
+        if not self.load:
+            self.init_env_config()
+
+        pattern = re.compile(r'\${(\w+)}')
+        data = []
+        with open(template_file_path) as f:
+            for line in f.readlines():
+                new_line = pattern.sub(self.replace_func, line)
+                data.append(new_line)
+
+        with open(out_file_path, 'w') as f:
+            f.write("".join(data))
+
+    def replace_func(self, match):
+        """
+        用env的变量替换${var}
+        """
+        value = match.group(1)
+        return self.env_config.get(value, None)
+
+    def __getitem__(self, item):
+        """
+        获取环境变量
+        """
+        if not self.load:
+            self.init_env_config()
+            self.load = True
+
+        return self.env_config[item]
+
+
+def auto_generate_config(conf_dir, config_dir, specific_env_mark=None, default_config_mark="default",
+                         template_mark="template"):
+    """
+    conf目录, config环境变量目录
+    """
+    config_file_list = os.listdir(config_dir)
+    default_config_file_path = None
+
+    # fetch default config
+    for config_file_name in config_file_list:
+        if default_config_mark in config_file_name:
+            default_config_file_path = os.path.join(config_dir, config_file_name)
+            break
+
+    # need default settings
+    assert default_config_file_path is not None
+
+    for config_file_name in config_file_list:
+        if default_config_mark in config_file_name:
+            continue
+        env_name = config_file_name.split(".")[0]
+        if specific_env_mark and specific_env_mark != env_name:
+            continue
+        config_manager = ConfigManager(default_config_file_path, os.path.join(config_dir, config_file_name))
+        config_dir_name = config_dir.split("/")[-1]
+        conf_dir_list = os.listdir(conf_dir)
+        for dir_name in conf_dir_list:
+            if dir_name == config_dir_name:
+                continue
+            cur_dir = os.path.join(conf_dir, dir_name)
+            cur_dir_file_list = os.listdir(cur_dir)
+            for file_name in cur_dir_file_list:
+                if template_mark in file_name:
+                    config_manager.render_config_file(os.path.join(cur_dir, file_name),
+                                                      os.path.join(cur_dir, file_name.replace(template_mark, env_name)))
+
+
+if __name__ == "__main__":
+    # auto generate specific batch config files: python config_manager.py conf conf/env [online]
+    # auto generate all batch config files: python config_manager.py conf conf/env
+    # python config_manager.py conf conf/env [online]
+    base_dir = sys.argv[1]
+    env_dir = sys.argv[2]
+    # if not assign then generate all
+    env_mark = sys.argv[3] if len(sys.argv) > 3 else None
+    auto_generate_config(base_dir, env_dir, env_mark)
